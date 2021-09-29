189de93bb85e03a5e90d1e2d1f7e94026279d45c
Andrew White <andrew.white@unboxed.co>
2021-09-29 06:55:58 +0100

    Add a mock api server for local development
    Data is stored in a db.json file in the project root which is git ignored.

---
+++ api.js
@@ -0,0 +1,44 @@
+const { BONUSCALC_SERVICE_API_KEY } = process.env
+
+const isAuthorized = (req) => {
+  return req.headers['x-api-key'] === BONUSCALC_SERVICE_API_KEY
+}
+
+const toJSONAPI = (type, attributes) => {
+  const id = attributes['id']
+  return { type, id, attributes }
+}
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    next()
+  } else {
+    res.status(401).json({
+      errors: [{ status: '401', title: 'Unauthorized' }],
+    })
+  }
+})
+
+router.render = (req, res) => {
+  const data = res.locals.data
+  const type = 'operatives'
+
+  if (Array.isArray(data)) {
+    res.jsonp({ data: data.map((item) => toJSONAPI(type, item)) })
+  } else {
+    res.jsonp({ data: toJSONAPI(type, data) })
+  }
+}
+
+server.use('/api/v1', router)
+
+server.listen(5001, () => {
+  console.log('JSON Server is running')
+})
