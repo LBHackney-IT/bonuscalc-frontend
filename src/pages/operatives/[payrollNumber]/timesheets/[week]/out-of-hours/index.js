9c866a40355a3485fada282063643268f61eb36d
Andrew White <andrew.white@unboxed.co>
2021-11-09 18:25:39 +0000

    Add the header to the out-of-hours page

--- src/pages/operatives/[payrollNumber]/timesheets/[week]/out-of-hours/index.js
+++ src/pages/operatives/[payrollNumber]/timesheets/[week]/out-of-hours/index.js
@@ -2,28 +2,53 @@ import PageContext from '@/components/PageContext'
 import BackButton from '@/components/BackButton'
 import OperativeSummary from '@/components/OperativeSummary'
 import OperativeTabs from '@/components/OperativeTabs'
-import NotFound from '@/components/NotFound'
+import OutOfHoursSummary from '@/components/OutOfHoursSummary'
 import Spinner from '@/components/Spinner'
+import NotFound from '@/components/NotFound'
+import { BonusPeriod } from '@/models'
 import { OPERATIVE_MANAGER_ROLE } from '@/utils/user'
-import { useOperative } from '@/utils/apiClient'
+import { useOperative, useTimesheet } from '@/utils/apiClient'
 
 const OperativePage = ({ query }) => {
   const { payrollNumber, week } = query
-  const { operative, isLoading, isError } = useOperative(payrollNumber)
+  const {
+    operative,
+    isLoading: isOperativeLoading,
+    isError: isOperativeError,
+  } = useOperative(payrollNumber)
 
-  if (isLoading) return <Spinner />
-  if (isError || !operative)
+  const {
+    timesheet,
+    isLoading: isTimesheetLoading,
+    isError: isTimesheetError,
+  } = useTimesheet(payrollNumber, week)
+
+  if (isOperativeLoading) return <Spinner />
+  if (isOperativeError || !operative)
     return (
       <NotFound
         message={`Couldn\u2019t find an operative with the payroll number ${payrollNumber}.`}
       />
     )
 
+  if (isTimesheetLoading) return <Spinner />
+  if (isTimesheetError || !timesheet)
+    return (
+      <NotFound
+        message={`Couldn\u2019t find a timesheet for the week beginning ${week}.`}
+      />
+    )
+
+  const bonusPeriod = BonusPeriod.forWeek(week)
+  const context = { operative, timesheet, week, bonusPeriod }
+
   return (
-    <PageContext.Provider value={{ operative, week }}>
+    <PageContext.Provider value={context}>
       <BackButton href="/" />
       <OperativeSummary />
-      <OperativeTabs current={3} />
+      <OperativeTabs current={3}>
+        <OutOfHoursSummary />
+      </OperativeTabs>
     </PageContext.Provider>
   )
 }
