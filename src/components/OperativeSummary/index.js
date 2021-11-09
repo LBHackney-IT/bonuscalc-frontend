65048dc2864a2e57113abcad162aea874f1397d0
Andrew White <andrew.white@unboxed.co>
2021-11-09 17:57:17 +0000

    Set the width of the labels to ensure they don't break

--- src/components/OperativeSummary/index.js
+++ src/components/OperativeSummary/index.js
@@ -10,25 +10,37 @@ const OperativeSummary = () => {
 
       <dl className="govuk-summary-list lbh-summary-list govuk-!-margin-top-5">
         <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Employee No</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Employee No
+          </dt>
           <dd className="govuk-summary-list__value">{operative.id}</dd>
-          <dt className="govuk-summary-list__key">Section / Team</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Section / Team
+          </dt>
           <dd className="govuk-summary-list__value">{operative.section}</dd>
         </div>
         <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Trade</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Trade
+          </dt>
           <dd className="govuk-summary-list__value">
             {operative.tradeDescription}
           </dd>
-          <dt className="govuk-summary-list__key">Scheme</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Scheme
+          </dt>
           <dd className="govuk-summary-list__value">
             {operative.schemeDescription}
           </dd>
         </div>
         <div className="govuk-summary-list__row">
-          <dt className="govuk-summary-list__key">Salary Band</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Salary Band
+          </dt>
           <dd className="govuk-summary-list__value">{operative.salaryBand}</dd>
-          <dt className="govuk-summary-list__key">Fixed Band</dt>
+          <dt className="govuk-summary-list__key govuk-!-width-one-third">
+            Fixed Band
+          </dt>
           <dd className="govuk-summary-list__value">
             {operative.fixedBand ? 'Yes' : 'No'}
           </dd>
