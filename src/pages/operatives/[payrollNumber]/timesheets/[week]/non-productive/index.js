51e658dcb3fd2a5471fa28c6e8b436f9a4a3714f
Andrew White <andrew.white@unboxed.co>
2021-11-16 10:29:02 +0000

    Extract operative weekly page to reusable component

--- src/pages/operatives/[payrollNumber]/timesheets/[week]/non-productive/index.js
+++ src/pages/operatives/[payrollNumber]/timesheets/[week]/non-productive/index.js
@@ -1,53 +1,10 @@
-import PageContext from '@/components/PageContext'
-import BackButton from '@/components/BackButton'
-import OperativeSummary from '@/components/OperativeSummary'
-import OperativeTabs from '@/components/OperativeTabs'
+import OperativePage from '@/components/OperativePage'
 import NonProductiveSummary from '@/components/NonProductiveSummary'
-import Spinner from '@/components/Spinner'
-import NotFound from '@/components/NotFound'
-import { BonusPeriod } from '@/models'
 import { OPERATIVE_MANAGER_ROLE } from '@/utils/user'
-import { useOperative, useTimesheet } from '@/utils/apiClient'
 
-const OperativePage = ({ query }) => {
-  const { payrollNumber, week } = query
-  const {
-    operative,
-    isLoading: isOperativeLoading,
-    isError: isOperativeError,
-  } = useOperative(payrollNumber)
-
-  const {
-    timesheet,
-    isLoading: isTimesheetLoading,
-    isError: isTimesheetError,
-  } = useTimesheet(payrollNumber, week)
-
-  if (isOperativeLoading) return <Spinner />
-  if (isOperativeError || !operative)
-    return (
-      <NotFound>
-        Couldn’t find an operative with the payroll number {payrollNumber}.
-      </NotFound>
-    )
-
-  if (isTimesheetLoading) return <Spinner />
-  if (isTimesheetError || !timesheet)
-    return <NotFound>Couldn’t find a timesheet for the date {week}.</NotFound>
-
-  const bonusPeriod = BonusPeriod.forWeek(week)
-  const context = { operative, timesheet, week, bonusPeriod }
-
-  return (
-    <PageContext.Provider value={context}>
-      <BackButton href="/" />
-      <OperativeSummary />
-      <OperativeTabs current={2}>
-        <NonProductiveSummary />
-      </OperativeTabs>
-    </PageContext.Provider>
-  )
-}
+const NonProductivePage = ({ query }) => (
+  <OperativePage query={query} tab={2} component={NonProductiveSummary} />
+)
 
 export const getServerSideProps = async (ctx) => {
   const { query } = ctx
@@ -59,6 +16,6 @@ export const getServerSideProps = async (ctx) => {
   }
 }
 
-OperativePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
+NonProductivePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
 
-export default OperativePage
+export default NonProductivePage
--- src/pages/operatives/[payrollNumber]/timesheets/[week]/overtime/index.js
+++ src/pages/operatives/[payrollNumber]/timesheets/[week]/overtime/index.js
@@ -1,53 +1,10 @@
-import PageContext from '@/components/PageContext'
-import BackButton from '@/components/BackButton'
-import OperativeSummary from '@/components/OperativeSummary'
-import OperativeTabs from '@/components/OperativeTabs'
+import OperativePage from '@/components/OperativePage'
 import OvertimeSummary from '@/components/OvertimeSummary'
-import Spinner from '@/components/Spinner'
-import NotFound from '@/components/NotFound'
-import { BonusPeriod } from '@/models'
 import { OPERATIVE_MANAGER_ROLE } from '@/utils/user'
-import { useOperative, useTimesheet } from '@/utils/apiClient'
 
-const OperativePage = ({ query }) => {
-  const { payrollNumber, week } = query
-  const {
-    operative,
-    isLoading: isOperativeLoading,
-    isError: isOperativeError,
-  } = useOperative(payrollNumber)
-
-  const {
-    timesheet,
-    isLoading: isTimesheetLoading,
-    isError: isTimesheetError,
-  } = useTimesheet(payrollNumber, week)
-
-  if (isOperativeLoading) return <Spinner />
-  if (isOperativeError || !operative)
-    return (
-      <NotFound>
-        Couldn’t find an operative with the payroll number {payrollNumber}.
-      </NotFound>
-    )
-
-  if (isTimesheetLoading) return <Spinner />
-  if (isTimesheetError || !timesheet)
-    return <NotFound>Couldn’t find a timesheet for the date {week}.</NotFound>
-
-  const bonusPeriod = BonusPeriod.forWeek(week)
-  const context = { operative, timesheet, week, bonusPeriod }
-
-  return (
-    <PageContext.Provider value={context}>
-      <BackButton href="/" />
-      <OperativeSummary />
-      <OperativeTabs current={4}>
-        <OvertimeSummary />
-      </OperativeTabs>
-    </PageContext.Provider>
-  )
-}
+const OvertimePage = ({ query }) => (
+  <OperativePage query={query} tab={4} component={OvertimeSummary} />
+)
 
 export const getServerSideProps = async (ctx) => {
   const { query } = ctx
@@ -59,6 +16,6 @@ export const getServerSideProps = async (ctx) => {
   }
 }
 
-OperativePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
+OvertimePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
 
-export default OperativePage
+export default OvertimePage
