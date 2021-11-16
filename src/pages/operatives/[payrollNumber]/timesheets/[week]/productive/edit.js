59c785d5dbb877b0b65c5a7728557e5a876dfcb0
Andrew White <andrew.white@unboxed.co>
2021-11-16 10:31:15 +0000

    Extract edit operative weekly page to reusable component

--- src/pages/operatives/[payrollNumber]/timesheets/[week]/productive/edit.js
+++ src/pages/operatives/[payrollNumber]/timesheets/[week]/productive/edit.js
@@ -1,76 +1,27 @@
-import PageContext from '@/components/PageContext'
-import BackButton from '@/components/BackButton'
+import EditOperativePage from '@/components/EditOperativePage'
 import EditProductive from '@/components/EditProductive'
-import Spinner from '@/components/Spinner'
-import NotFound from '@/components/NotFound'
-import { BonusPeriod } from '@/models'
-import { OPERATIVE_MANAGER_ROLE } from '@/utils/user'
 import { compareStrings } from '@/utils/string'
-import {
-  useOperative,
-  usePayElementTypes,
-  useTimesheet,
-} from '@/utils/apiClient'
+import { OPERATIVE_MANAGER_ROLE } from '@/utils/user'
 
-const OperativePage = ({ query }) => {
-  const { payrollNumber, week } = query
-  const {
-    operative,
-    isLoading: isOperativeLoading,
-    isError: isOperativeError,
-  } = useOperative(payrollNumber)
+const EditProductivePage = ({ query }) => {
+  const selectPayElements = (timesheet) => {
+    return timesheet.adjustmentPayElements
+  }
 
-  const {
-    timesheet,
-    isLoading: isTimesheetLoading,
-    isError: isTimesheetError,
-  } = useTimesheet(payrollNumber, week)
-
-  const {
-    payElementTypes: allPayElementTypes,
-    isLoading: isPayElementTypesLoading,
-    isError: isPayElementTypesError,
-  } = usePayElementTypes()
-
-  if (isOperativeLoading) return <Spinner />
-  if (isOperativeError || !operative)
-    return (
-      <NotFound>
-        Couldn’t find an operative with the payroll number {payrollNumber}.
-      </NotFound>
-    )
-
-  if (isTimesheetLoading) return <Spinner />
-  if (isTimesheetError || !timesheet)
-    return <NotFound>Couldn’t find a timesheet for the date {week}.</NotFound>
-
-  if (isPayElementTypesLoading) return <Spinner />
-  if (isPayElementTypesError || !allPayElementTypes)
-    return <NotFound>Couldn’t fetch the list of pay element types.</NotFound>
-
-  const bonusPeriod = BonusPeriod.forWeek(week)
-  const baseUrl = `/operatives/${operative.id}`
-  const backUrl = `${baseUrl}/timesheets/${week}/productive`
-
-  const payElements = timesheet.adjustmentPayElements
-  const payElementTypes = allPayElementTypes
-    .filter((pet) => pet.adjustment && pet.selectable)
-    .sort((a, b) => compareStrings(a.description, b.description))
-
-  const context = {
-    operative,
-    timesheet,
-    payElements,
-    payElementTypes,
-    bonusPeriod,
-    week,
+  const selectPayElementTypes = (payElementTypes) => {
+    return payElementTypes
+      .filter((pet) => pet.adjustment && pet.selectable)
+      .sort((a, b) => compareStrings(a.description, b.description))
   }
 
   return (
-    <PageContext.Provider value={context}>
-      <BackButton href={backUrl} />
-      <EditProductive />
-    </PageContext.Provider>
+    <EditOperativePage
+      query={query}
+      tab="productive"
+      selectPayElements={selectPayElements}
+      selectPayElementTypes={selectPayElementTypes}
+      component={EditProductive}
+    />
   )
 }
 
@@ -84,6 +35,6 @@ export const getServerSideProps = async (ctx) => {
   }
 }
 
-OperativePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
+EditProductivePage.permittedRoles = [OPERATIVE_MANAGER_ROLE]
 
-export default OperativePage
+export default EditProductivePage
