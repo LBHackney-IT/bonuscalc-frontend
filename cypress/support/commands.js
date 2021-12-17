90016d5246c09eedfd85de685a239935ceedd1af
Andrew White <andrew.white@unboxed.co>
2021-12-17 04:07:49 +0000

    Switch to dotenv-flow for environment variables
    This allows us to have per-environment files for dev, test, etc.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,16 +27,19 @@
 import 'cypress-audit/commands'
 
 Cypress.Commands.add('login', () => {
-  const gssoTestKey = Cypress.env('GSSO_TEST_KEY')
+  const GSSO_TEST_KEY = Cypress.env('GSSO_TEST_KEY')
+  const GSSO_TOKEN_NAME = Cypress.env('GSSO_TOKEN_NAME')
 
   cy.getCookies().should('be.empty')
-  cy.setCookie('hackneyToken', gssoTestKey)
-  cy.getCookie('hackneyToken').should('have.property', 'value', gssoTestKey)
+  cy.setCookie(GSSO_TOKEN_NAME, GSSO_TEST_KEY)
+  cy.getCookie(GSSO_TOKEN_NAME).should('have.property', 'value', GSSO_TEST_KEY)
 })
 
 Cypress.Commands.add('logout', () => {
+  const GSSO_TOKEN_NAME = Cypress.env('GSSO_TOKEN_NAME')
+
   cy.get('#signout').contains('Sign out')
-  cy.clearCookie('hackneyToken')
+  cy.clearCookie(GSSO_TOKEN_NAME)
 
   cy.getCookies().should('be.empty')
 })
