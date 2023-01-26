fdd23c8598d00545a6d3a4bac3087f08a17f47fc
Dave Dean <david.dean@hackney.gov.uk>
2023-01-26 12:50:42 +0000

    Fix tests

--- cypress/integration/search-page.spec.js
+++ cypress/integration/search-page.spec.js
@@ -394,7 +394,7 @@ describe('Search page', () => {
               })
             cy.get(':nth-child(4)').contains('2 Somewhere Street')
             cy.get(':nth-child(5)').contains('19/10/2021')
-            cy.get(':nth-child(6)').contains('£21.98')
+            cy.get(':nth-child(6)').contains('£22.86')
             cy.get(':nth-child(7)')
               .contains('a', '3')
               .should((link) => {
