b6108024be2c381df8591f6fcc1a8c92e753bcac
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-09-26 09:34:36 +0100

    Try removing axios mock

--- setupTests.js
+++ setupTests.js
@@ -15,12 +15,40 @@ jest.mock('@sentry/nextjs', () => {
   }
 })
 
+
+
+
+
 // Force the axios default adapter to Node's http module.
 // Otherwise all the requests will use the XHR adapter and
 // will run pre-flight CORS checks.
-jest.mock('axios', () => {
-  const axios = jest.requireActual('axios')
-  axios.defaults.adapter = jest.requireActual('axios/lib/adapters/http')
+// jest.mock('axios', () => {
+//   const axios = jest.requireActual('axios')
+//   axios.defaults.adapter = jest.requireActual('axios/lib/adapters/http')
 
-  return axios
-})
+//   return axios
+// })
+
+
+// jest.mock('axios', () => {
+//   const axios = jest.requireActual('axios/dist/node/axios.cjs')
+//   return axios
+// })
+
+// jest.mock('axios', () => {
+//   try {
+//     // Try the CommonJS version first
+//     const axios = jest.requireActual('axios/dist/node/axios.cjs')
+//     return axios
+//   } catch (error) {
+//     // Fallback to the regular require if CJS version doesn't exist
+//     const axios = jest.requireActual('axios')
+//     // Set a default adapter that works in Node.js
+//     if (axios.defaults) {
+//       const http = jest.requireActual('http')
+//       const https = jest.requireActual('https')
+//       axios.defaults.adapter = jest.requireActual('axios/lib/adapters/http.js')
+//     }
+//     return axios
+//   }
+// })
