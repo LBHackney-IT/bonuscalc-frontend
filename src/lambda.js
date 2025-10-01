17d7c1502f7145dafe07de8db23d6b1d8abc61ce
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-10-01 11:02:46 +0100

    Try waiting for handler to initiate

--- src/lambda.js
+++ src/lambda.js
@@ -2,11 +2,25 @@ const server = require('restana')()
 const app = require('next')({ dev: false })
 const files = require('serve-static')
 const path = require('path')
-const nextRequestHandler = app.getRequestHandler()
+
+
+const getHandler = async () => {
+    await app.prepare()
+    
+    const nextRequestHandler = app.getRequestHandler()
+
+    return nextRequestHandler;
+    
+    // handler = require('serverless-http')(server);
+    
+}
+
+const nextRequestHandler = await getHandler()
 
 server.use(files(path.join(__dirname, 'build')))
 server.use(files(path.join(__dirname, 'public')))
 
 server.all('*', (req, res) => nextRequestHandler(req, res))
+
 
 module.exports.handler = require('serverless-http')(server)
