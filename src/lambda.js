fa4ad7f06e5261e115076d0bdcea2691578cd175
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-10-01 12:38:03 +0100

    Fix deployment for Next 13 (#149)
    * Toggle pipeline
    
    * cleanup serverless
    
    * Try waiting for handler to initiate
    
    * LF
    
    * Update lambda.js
    
    * revert pipeline

--- src/lambda.js
+++ src/lambda.js
@@ -2,11 +2,20 @@ const server = require('restana')()
 const app = require('next')({ dev: false })
 const files = require('serve-static')
 const path = require('path')
-const nextRequestHandler = app.getRequestHandler()
 
-server.use(files(path.join(__dirname, 'build')))
-server.use(files(path.join(__dirname, 'public')))
+const getHandler = async () => {
+  await app.prepare()
+  const nextRequestHandler = app.getRequestHandler()
 
-server.all('*', (req, res) => nextRequestHandler(req, res))
+  server.use(files(path.join(__dirname, 'build')))
+  server.use(files(path.join(__dirname, 'public')))
 
-module.exports.handler = require('serverless-http')(server)
+  server.all('*', (req, res) => nextRequestHandler(req, res))
+
+  return require('serverless-http')(server)
+}
+
+module.exports.handler = async (event, context) => {
+  const serverlessHandler = await getHandler()
+  return serverlessHandler(event, context)
+}
