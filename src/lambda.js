5ea82e7950af7320a665fa33689a10e697c00617
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-10-01 11:19:45 +0100

    LF

--- src/lambda.js
+++ src/lambda.js
@@ -3,16 +3,14 @@ const app = require('next')({ dev: false })
 const files = require('serve-static')
 const path = require('path')
 
-
 const getHandler = async () => {
-    await app.prepare()
-    
-    const nextRequestHandler = app.getRequestHandler()
+  await app.prepare()
 
-    return nextRequestHandler;
-    
-    // handler = require('serverless-http')(server);
-    
+  const nextRequestHandler = app.getRequestHandler()
+
+  return nextRequestHandler
+
+  // handler = require('serverless-http')(server);
 }
 
 const nextRequestHandler = await getHandler()
@@ -21,6 +19,5 @@ server.use(files(path.join(__dirname, 'build')))
 server.use(files(path.join(__dirname, 'public')))
 
 server.all('*', (req, res) => nextRequestHandler(req, res))
-
 
 module.exports.handler = require('serverless-http')(server)
