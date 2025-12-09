20a70142254548f7aea8ad4418c00089723a220a
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-12-09 10:52:46 +0000

    prettier

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ async function headers() {
         },
       ],
     },
-  ];
+  ]
 }
 
 const moduleExports = {
--- src/lambda.js
+++ src/lambda.js
@@ -1,40 +1,40 @@
-const server = require('restana')();
-const files = require('serve-static');
-const path = require('path');
+const server = require('restana')()
+const files = require('serve-static')
+const path = require('path')
 
-const standaloneDir = path.join(__dirname, '../build/_next/standalone');
-const Next = require(path.join(standaloneDir, 'node_modules/next'));
+const standaloneDir = path.join(__dirname, '../build/_next/standalone')
+const Next = require(path.join(standaloneDir, 'node_modules/next'))
 
 // https://github.com/vercel/next.js/issues/64031#issuecomment-2078708340
 const { config } = require(path.join(
   standaloneDir,
   'build/_next/required-server-files.json'
-));
-process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(config);
+))
+process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(config)
 
 const app = Next({
   dev: false,
   dir: standaloneDir,
   conf: config,
-});
+})
 
-let isReady = false;
-let nextRequestHandler;
+let isReady = false
+let nextRequestHandler
 
 async function init() {
   if (!isReady) {
-    await app.prepare();
-    nextRequestHandler = app.getRequestHandler();
-    isReady = true;
+    await app.prepare()
+    nextRequestHandler = app.getRequestHandler()
+    isReady = true
   }
 }
 
-server.use(files(path.join(__dirname, '../build')));
-server.use(files(path.join(__dirname, '../public')));
+server.use(files(path.join(__dirname, '../build')))
+server.use(files(path.join(__dirname, '../public')))
 
 server.all('*', async (req, res) => {
-  await init();
-  return nextRequestHandler(req, res);
-});
+  await init()
+  return nextRequestHandler(req, res)
+})
 
-module.exports.handler = require('serverless-http')(server);
+module.exports.handler = require('serverless-http')(server)
