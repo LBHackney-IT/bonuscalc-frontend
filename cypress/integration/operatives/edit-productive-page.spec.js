e96ecaed437f440ecae30da6427e5dd0e1c3d1d4
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-12-10 11:14:04 +0000

    Update remaining tests

--- cypress/integration/operatives/edit-productive-page.spec.js
+++ cypress/integration/operatives/edit-productive-page.spec.js
@@ -150,7 +150,7 @@ describe('Productive page', () => {
           { statusCode: 200, body: {} }
         ).as('update_timesheet')
 
-        cy.get('#confirm-button').click()
+        cy.contains("Confirm").click()
 
         cy.wait('@update_timesheet')
 
